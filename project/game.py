341d067e7c74935483ba348f928a15c4d354815c
diogoemoreira <diogo.e.moreira@hotmail.com>
2022-01-17 12:59:44 +0000

    now the user can keep the key pressed
    The user can now stay with the same key pressed and it will update its position

--- project/game.py
+++ project/game.py
@@ -24,57 +24,42 @@ class Game:
         self.eggs = []
         self.food = []
         
-        self.game_manager = GameManager(SCALE, self.floor)
+        self.game_manager = GameManager(SCALE, self.floor, dict()) #change dict to the ladder dictionary
 
         self.game_manager.add_observer(self.player)
 
     def loop(self):
         running = True
 
-        pygame.time.set_timer(UPDATE, 45)
-
         #Event handler
         while running:
-
-            while True:
-                event = pygame.event.poll() #receive an event from the poll of events
-                if event.type == pygame.NOEVENT: #no events triggered
-                    break
-                
+            self.clock.tick(30)
+            for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
                     break
 
                 elif event.type == pygame.KEYDOWN: #if the player pressed a key
-                    if event.key == pygame.K_UP:
-                        self.player.up()
-                    elif event.key == pygame.K_DOWN:
-                        self.player.down()
-                    elif event.key == pygame.K_LEFT:
-                        self.player.left()
-                    elif event.key == pygame.K_RIGHT:
-                        self.player.right()
-                    elif event.key == pygame.K_SPACE:
+                    if event.key == pygame.K_SPACE:
                         self.player.jump()
-                elif event.type == UPDATE:
-                    if not self.game_manager.floor_collide(self.player):
-                        self.player.update()
+                
+            keys = pygame.key.get_pressed()    
+            if keys[pygame.K_UP]:
+                self.player.up()
+            elif keys[pygame.K_DOWN]:
+                self.player.down()
+            if keys[pygame.K_LEFT]:
+                self.player.left()
+            elif keys[pygame.K_RIGHT]:
+                self.player.right()
+            #confirm if the characters already have floor beneath them
+            #if they have, dont update their gravity
+            if not self.game_manager.floor_collide(self.player):
+                self.player.update() #gravity update
 
-                    for enemy in self.enemies:
-                        if not self.game_manager.floor_collide(enemy):
-                            enemy.update() #update enemies' position
-                    '''
-                    #Update
-                    self.player.update() #update player's position
-                    for enemy in self.enemies:
-                        enemy.update() #update enemies' position
-
-                    #Collisions
-                    self.player.collide(self.enemies)
-                    self.player.collide(self.eggs)
-                    self.player.collide(self.food)
-                    '''
-            #Test clock to see if the frame time already passed
+            for enemy in self.enemies:
+                if not self.game_manager.floor_collide(enemy):
+                    enemy.update() #gravity update
 
             #Render
             self.display.fill("white")
@@ -85,10 +70,10 @@ class Game:
             for pos in self.floor.keys():
                 self.floor.get(pos).render(self.display, pos[0], pos[1])
             '''
-            self.player.render(self.display)
-            self.enemies.render(self.display)
-            self.eggs.render(self.display)
-            self.food.render(self.display)
+            for egg in self.eggs:
+                egg.render(self.display)
+            for food in self.food:
+                food.render(self.display)
             '''
 
             pygame.display.flip()
@@ -99,7 +84,7 @@ if __name__=="__main__":
     floor = Floor(SCALE)
 
     floor_dic = dict()
-    for x in range(1,WIDTH):
+    for x in range(1,WIDTH-2):
         floor_dic[(x,HEIGHT-2)] = floor
 
     g = Game(floor_dic)
--- project/game_manager.py
+++ project/game_manager.py
@@ -1,10 +1,11 @@
 from observable import Observable
 
 class GameManager(Observable):
-    def __init__(self,scale, floor:dict):
+    def __init__(self,scale, floor:dict, ladders:dict):
         super().__init__()
         self.scale = scale
         self.floor = floor
+        self.ladders = ladders
 
     def collide(self, ax,ay , bx,by):
         #confirm collision between a and b
@@ -13,7 +14,14 @@ class GameManager(Observable):
         return False
     
     def floor_collide(self, character):
+        #confirm if the character has a floor tile beneath him
+        #notify the character if it is standing on the floor
         if self.floor.get((character.x, character.y+1)):
             self.notify(character, "on_floor")
             return True
         return False
+    
+    def on_ladder(self, character):
+        #notify the character if it is on top of a ladder
+        if self.ladders.get((character.x, character.y)):
+            self.notify(character, "on_ladder")
